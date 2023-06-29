3e692017cdafd023340db894bcf53c058e04d740
tbvjchvkfl <anfmdehdnjs@gmail.com>
2023-06-30 00:31:54 +0900

    초기 커밋.

---
+++ Main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+int Ffunction(int a);
+
+
+int main()
+{
+	Ffunction(10);
+}
+
+int Ffunction(int a)
+{
+	if (a < 0)
+	{
+		cout << endl;
+		return;
+	}
+
+	cout << a << ",";
+	Ffunction(a - 1);
+
+
+	return 0;
+}
